d7a39bfb6d8b2a66cf417d343f9e19de820380ca
mohamd-khairy <m.khairy@evntoo.com>
2020-05-12 18:28:01 +0200

    changes

--- src/app/modules/profile/about/about.component.ts
+++ src/app/modules/profile/about/about.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileService } from '../profile.service';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-about',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  public errorMsg = '';
+  public successMsg = '';
 
-  constructor() { }
+  public jobData ;
+  // private _job = new BehaviorSubject<Array<any>>(null);
+  // job$ = this._job.asObservable();
+
+  constructor(private profileService: ProfileService) { }
 
   ngOnInit(): void {
+    this.get_job_data();
   }
 
+  get_job_data(){
+    this.profileService.get_job_details_api().subscribe(
+      (res) => { 
+        console.log('the data:' , res);
+        // this._job.next(JSON.parse(JSON.stringify(res)).data); 
+        this.jobData = JSON.parse(JSON.stringify(res)).data; 
+        this.successMsg = "Data returned Successfully";
+      },
+      (err) => {
+        console.log(err);
+        this.errorMsg = err.status == 422 ? err.error.errors[Object.keys(err.error.errors)[0]][0] : err.error.message ;
+      } 
+    );
+  }
 }
---
+++ src/app/modules/profile/profile.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
+import { AuthService } from 'src/app/auth/auth.service';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProfileService {
+
+  constructor(private http: HttpClient,private router: Router,private authService: AuthService) { }
+
+  get_job_details_api()
+  {
+    return this.http.get(environment.apiUrl + 'job?user_id='+this.authService.userData.id)
+    .pipe(catchError(this.errorHandler));
+  }
+
+  get_profile_api()
+  {
+    return this.http.get(environment.apiUrl + 'profile/'+this.authService.userData.id)
+    .pipe(catchError(this.errorHandler));
+  }
+
+  get_skills_api()
+  {
+    return this.http.get(environment.apiUrl + 'skill?user_id='+this.authService.userData.id)
+    .pipe(catchError(this.errorHandler));
+  }
+
+  errorHandler(error: HttpErrorResponse){
+    return throwError(error);
+  }
+}
