2ebd13109b68bc8366c1613a521166ac6e4dd22c
mohamed.khairy <mohamed.khairy@orchtech.com>
2020-06-11 13:31:25 +0200

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,3 +2,13 @@ export const environment = {
   production: true,
   apiUrl: 'https://infinite-atoll-41509.herokuapp.com/api/v1/'
 };
+export const firebaseConfig = {
+  apiKey: "AIzaSyD-iENrntrSnZ23z0RF9BBM3SZ92oSKxoc",
+  authDomain: "my-mentor-2cff4.firebaseapp.com",
+  databaseURL: "https://my-mentor-2cff4.firebaseio.com",
+  projectId: "my-mentor-2cff4",
+  storageBucket: "my-mentor-2cff4.appspot.com",
+  messagingSenderId: "194532485887",
+  appId: "1:194532485887:web:92beb9a035ee6cd6663aca",
+  measurementId: "G-1W5H6ZV4K3"
+};
